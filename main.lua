7f4c2afe5f0c463b4e6410eb62c6df821a090ce7
Aythreuk <53133375+Aythreuk@users.noreply.github.com>
2021-01-05 22:42:43 -0600

    Update main.lua
    Did some fading in and out with the music, created a rectangle and then was in the middle of trying to do something with the picker wheel widget.

--- main.lua
+++ main.lua
@@ -10,19 +10,21 @@ local widget = require("widget")
 -- Music and audio
 musicTrack1 = audio.loadStream("testmusic/Stella.mp3")
 audio.reserveChannels(1)
+audio.setVolume( 0.0, {channel = 1} )
 
 -- Start music function
 local function startMusic ( event )
-  if (event.phase == "began") then
+  if (event.phase == "ended") then
     audio.play(musicTrack1, {channel = 1} )
+    audio.fade({channel = 1, time = 2000, volume = 1.0})
   end
 end
 
 -- Create start music button
 local button1 = widget.newButton (
 {
-  left = 50,
-  top = 50,
+  left = 0,
+  top = 0,
   id = "button1",
   label = "Start music",
   onEvent = startMusic
@@ -31,20 +33,37 @@ local button1 = widget.newButton (
 
 -- Stop music function
 local function stopMusic ( event )
-  print(audio.isChannelActive(1))
   if (event.phase == "ended") then
-    audio.stop( 1 )
-    audio.dispose(musicTrack1)
+    audio.fadeOut({channel = 1, time = 2000})
   end
 end
 
 -- Create stop music button
 local button2 = widget.newButton (
 {
-  left = 50,
-  top = 150,
+  left = 0,
+  top = 50,
   id = "button2",
   label = "Stop music",
   onEvent = stopMusic
 }
 )
+
+--[[ Let's create a rectangle or something
+local shape1 = display.newRect (100, 150, 100, 100)
+shape1:setFillColor(0.8, 0.4, 0.4, 1.0)
+
+-- Picker pickerTable
+local pickerTable = {
+"Red",
+"Green",
+"Blue"
+}
+
+-- Let's try use the PickerWheelWidget
+local picker1 = widget.newPickerWheel ({
+top = 200,
+left = 0,
+columns = pickerTable,
+})
+--]]
