0f5e53a6344bd67caec94756487235a4676ab115
Aythreuk <53133375+Aythreuk@users.noreply.github.com>
2021-01-06 22:30:42 -0600

    Sprite & imagesheet added
    Also tried to make clicking on the clock change the playing sequence except I was experiencing difficulties.

--- main.lua
+++ main.lua
@@ -7,10 +7,105 @@
 -- Load libraries and modules
 local widget = require("widget")
 
+-- Display groups etc.
+local displayGroup1 = display.newGroup()
+
 -- Music and audio
 musicTrack1 = audio.loadStream("testmusic/Stella.mp3")
 audio.reserveChannels(1)
 audio.setVolume( 0.0, {channel = 1} )
+
+-- Image sheet
+local sheetOptions =
+{
+  frames = {
+    {
+      -- ani1
+      x=1,
+      y=1,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani2
+      x=1,
+      y=303,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani3
+      x=1,
+      y=605,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani4
+      x=303,
+      y=1,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani5
+      x=605,
+      y=1,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani6
+      x=303,
+      y=303,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani7
+      x=303,
+      y=605,
+      width=300,
+      height=300,
+
+    },
+    {
+      -- ani8
+      x=605,
+      y=303,
+      width=300,
+      height=300,
+
+    },
+  },
+}
+local imageSheet = graphics.newImageSheet( "sheet1.png", sheetOptions )
+
+-- Animation sequence table
+local clockSequence = {
+  {
+    name = "clockFwd",
+    start =  1,
+    count = 8,
+    time = 800,
+    loopCount = 0,
+    loopDirection = "forward"
+  },
+  {
+    name = "clockBkwd",
+    start =  1,
+    count = 8,
+    time = 800,
+    loopCount = 0,
+    loopDirection = "backward"
+  },
+}
 
 -- Start music function
 local function startMusic ( event )
@@ -49,21 +144,30 @@ local button2 = widget.newButton (
 }
 )
 
---[[ Let's create a rectangle or something
+--Let's create a rectangle or something
 local shape1 = display.newRect (100, 150, 100, 100)
 shape1:setFillColor(0.8, 0.4, 0.4, 1.0)
 
--- Picker pickerTable
-local pickerTable = {
-"Red",
-"Green",
-"Blue"
-}
+-- clock function
+local function clockDirecChange ( event )
+  local thisSprite = event.target
+  if (thisSprite.sequence == "clockFwd") then
+    print(thisSprite.sequence)
+    thisSprite:setSequence("clockBkwd")
+    thisSprite:play()
+  else
+    print(thisSprite.sequence)
+    thisSprite:setSequence("clockFwd")
+    thisSprite:play()
+  end
+end
 
--- Let's try use the PickerWheelWidget
-local picker1 = widget.newPickerWheel ({
-top = 200,
-left = 0,
-columns = pickerTable,
-})
---]]
+-- Create clock sprite
+local sprite = display.newSprite( imageSheet, clockSequence)
+sprite.x = 125
+sprite.y = 350
+sprite.xScale = 0.50
+sprite.yScale = 0.50
+sprite:setSequence("clockFwd")
+sprite:play()
+sprite:addEventListener("touch", clockDirecChange)
